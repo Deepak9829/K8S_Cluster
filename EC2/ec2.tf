81847fc888c59600676f49be608fd09be875d83c
Deepak Saini <deepak.saini@tricog.com>
2023-02-27 00:51:34 +0530

    cluster added

--- EC2/ec2.tf
+++ EC2/ec2.tf
@@ -130,7 +130,7 @@ resource "local_file" "ipaddr" {
 resource "null_resource" "nulllocal3" {
   depends_on = [local_file.ipaddr]
   provisioner "local-exec" {
-    command = "ansible-playbook /home/deepaksaini/K8S_MultiNodeCluster/Ansible-Playbook/cluster.yml -i /home/deepaksaini/K8S_MultiNodeCluster/Ansible-Playbook/k8s.ini"
+    command = "ansible-playbook ../Ansible-Playbook/cluster.yml -i ../Ansible-Playbook/k8s.ini"
   }
 
 }
